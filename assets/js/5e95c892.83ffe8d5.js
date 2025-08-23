029c72b5ff0716f9755fac0d84624b72d6a85541
Heap-Hop <csf@enbop.com>
2025-08-23 20:38:12 +0900

    Deploy website - based on dbbdf39e5bf4615e712ac422352585985d357792

---
+++ assets/js/5e95c892.83ffe8d5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfungi_site=self.webpackChunkfungi_site||[]).push([[9647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>f});r(6540);var u=r(4164),n=r(7559),t=r(5500),a=r(2831),c=r(1656),i=r(4848);function f(e){return(0,i.jsx)(t.e3,{className:(0,u.A)(n.G.wrapper.docsPages),children:(0,i.jsx)(c.A,{children:(0,a.v)(e.route.routes)})})}}}]);
